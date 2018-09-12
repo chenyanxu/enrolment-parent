72ba93b2f38e197d8a00c9a83330d4473e10f0c0
fengweibin <tx_xinxin2006@163.com>
2018-09-12 13:49:45 +0800

    删除测试文件

--- question-parent/enrolment-question-dao/src/main/java/com/kalix/enrolment/question/dao/TestBeanDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.kalix.enrolment.question.dao;
-
-import com.kalix.enrolment.question.api.dao.ITestBeanDao;
-import com.kalix.enrolment.question.entities.TestBean;
-
-public class TestBeanDaoImpl extends BaseBeanDao<TestBean, Long> implements ITestBeanDao {
-
-}
