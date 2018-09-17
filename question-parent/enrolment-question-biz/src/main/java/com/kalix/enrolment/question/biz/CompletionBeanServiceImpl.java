23b89fda4c1d2b8758bbd3b0e2e29d58658f2d29
fengweibin <tx_xinxin2006@163.com>
2018-09-17 14:28:21 +0800

    加入逻辑删除

--- question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/CompletionBeanServiceImpl.java
+++ question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/CompletionBeanServiceImpl.java
@@ -3,10 +3,10 @@ package com.kalix.enrolment.question.biz;
 import com.kalix.enrolment.question.api.biz.ICompletionBeanService;
 import com.kalix.enrolment.question.api.dao.ICompletionBeanDao;
 import com.kalix.enrolment.question.entities.CompletionBean;
-import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
+import com.kalix.framework.extend.impl.biz.LogicDeleteGenericBizServiceImpl;
 
 /**
  * Created by Administrator_ on 2018/9/6.
  */
-public class CompletionBeanServiceImpl extends ShiroGenericBizServiceImpl<ICompletionBeanDao, CompletionBean> implements ICompletionBeanService {
+public class CompletionBeanServiceImpl extends LogicDeleteGenericBizServiceImpl<ICompletionBeanDao, CompletionBean> implements ICompletionBeanService {
 }
