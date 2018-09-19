6eacf054a6138b6b90f7f51d25dd64258c718ace
houqingjie <150091225@qq.com>
2018-09-19 09:48:04 +0800

    新增下载模板资源及其访问服务

--- question-parent/enrolment-question-rest/src/main/java/com/kalix/enrolment/question/rest/internal/InitActivator.java
+++ question-parent/enrolment-question-rest/src/main/java/com/kalix/enrolment/question/rest/internal/InitActivator.java
@@ -1,9 +1,35 @@
 package com.kalix.enrolment.question.rest.internal;
 
-import com.kalix.framework.core.api.osgi.BaseBundleActivator;
+import com.kalix.framework.core.api.osgi.KalixBundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
 
 /**
  * Created by sunlf on 14-3-23.
  */
-public class InitActivator extends BaseBundleActivator {
+public class InitActivator extends KalixBundleActivator {
+    private ServiceReference reference;
+    private HttpService httpService;
+
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        super.start(bundleContext);
+
+        reference = bundleContext.getServiceReference(HttpService.class.getName());
+        httpService = (HttpService) bundleContext.getService(reference);
+        httpService.registerResources(contextPath + "/enrolment/question/excel", "/excel", null);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        super.stop(bundleContext);
+
+        if (reference != null)
+            bundleContext.ungetService(reference);
+        if (httpService != null) {
+            httpService.unregister(contextPath + "/enrolment/question/excel");
+        }
+    }
 }
