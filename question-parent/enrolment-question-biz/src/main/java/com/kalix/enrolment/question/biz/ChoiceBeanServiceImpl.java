12434a119e870f830ddc7af57e47ea085808518d
houqingjie <150091225@qq.com>
2018-09-20 10:59:18 +0800

    新增默认排序功能

--- question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/ChoiceBeanServiceImpl.java
+++ question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/ChoiceBeanServiceImpl.java
@@ -3,10 +3,20 @@ package com.kalix.enrolment.question.biz;
 import com.kalix.enrolment.question.api.biz.IChoiceBeanService;
 import com.kalix.enrolment.question.api.dao.IChoiceBeanDao;
 import com.kalix.enrolment.question.entities.ChoiceBean;
+import com.kalix.framework.core.api.persistence.JsonData;
+import com.kalix.framework.core.util.StringUtils;
 import com.kalix.framework.extend.impl.biz.LogicDeleteGenericBizServiceImpl;
 
 /**
  * Created by angyanming at 2018-09-13
  */
 public class ChoiceBeanServiceImpl extends LogicDeleteGenericBizServiceImpl<IChoiceBeanDao, ChoiceBean> implements IChoiceBeanService {
+
+    @Override
+    public JsonData getAllEntityByQuery(Integer page, Integer limit, String jsonStr, String sort) {
+        if (StringUtils.isEmpty(sort)) {
+            sort = "[{'property': 'delFlag', 'direction': 'ASC'}]";
+        }
+        return super.getAllEntityByQuery(page, limit, jsonStr, sort);
+    }
 }
--- question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/CompletionBeanServiceImpl.java
+++ question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/CompletionBeanServiceImpl.java
@@ -3,12 +3,20 @@ package com.kalix.enrolment.question.biz;
 import com.kalix.enrolment.question.api.biz.ICompletionBeanService;
 import com.kalix.enrolment.question.api.dao.ICompletionBeanDao;
 import com.kalix.enrolment.question.entities.CompletionBean;
+import com.kalix.framework.core.api.persistence.JsonData;
+import com.kalix.framework.core.util.StringUtils;
 import com.kalix.framework.extend.impl.biz.LogicDeleteGenericBizServiceImpl;
-import org.xm.Similarity;
 
 /**
  * Created by Administrator_ on 2018/9/6.
  */
 public class CompletionBeanServiceImpl extends LogicDeleteGenericBizServiceImpl<ICompletionBeanDao, CompletionBean> implements ICompletionBeanService {
 
+    @Override
+    public JsonData getAllEntityByQuery(Integer page, Integer limit, String jsonStr, String sort) {
+        if (StringUtils.isEmpty(sort)) {
+            sort = "[{'property': 'delFlag', 'direction': 'ASC'}]";
+        }
+        return super.getAllEntityByQuery(page, limit, jsonStr, sort);
+    }
 }
