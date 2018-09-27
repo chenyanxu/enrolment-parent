82117c51c511d31cf4fa453de8f88b83a26399a8
houqingjie <150091225@qq.com>
2018-09-27 15:16:51 +0800

    修改单个排重服务bug，并格式化代码

--- question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/RepeatedDtoServiceImpl.java
+++ question-parent/enrolment-question-biz/src/main/java/com/kalix/enrolment/question/biz/RepeatedDtoServiceImpl.java
@@ -49,7 +49,7 @@ public class RepeatedDtoServiceImpl implements IRepeatedBeanService {
         JsonData jsondata = new JsonData();
         List<RepeatedCountDTO> repeateList = new ArrayList<RepeatedCountDTO>();
         List<Object> referenceList = new ArrayList<Object>();
-        //List<CompletionBean> completionlist =completionBeanDao.getAll();
+        //List<CompletionBean> completionlist = completionBeanDao.getAll();
         String hql = "select c from CompletionBean c where c.repeatedFlag='0' and c.delFlag='0'";
         List<CompletionBean> completionlist = (List<CompletionBean>) completionBeanDao.find(hql);
         referenceList.addAll(completionlist);
@@ -268,8 +268,6 @@ public class RepeatedDtoServiceImpl implements IRepeatedBeanService {
 
                         referenceList.remove(verse_child);
                     }
-
-
                 }
             }
             if (dtoList != null && dtoList.size() > 0) {
@@ -292,39 +290,35 @@ public class RepeatedDtoServiceImpl implements IRepeatedBeanService {
     public JsonData doRepeate(RepeatedDTO c_repeatedDTO) {
         String stem = c_repeatedDTO.getStem();
         String type = c_repeatedDTO.getQuestionType();
-        Long questionid=c_repeatedDTO.getQuestionId();
+        Long questionid = c_repeatedDTO.getQuestionId();
         JsonData jsondata = new JsonData();
         List<RepeatedCountDTO> repeateList = new ArrayList<RepeatedCountDTO>();
         List<Object> referenceList = new ArrayList<Object>();
         List<Object> completionlist = null;
         String hql = "";
-        //List<CompletionBean> completionlist =completionBeanDao.getAll();
-        if("CompletionBean".equals(type)){
-            if(questionid!=null&&questionid>0)
-            {
-                hql = "select c from CompletionBean c where  and c.delFlag='0' and id<>"+questionid ;
-            }else {
-                hql = "select c from CompletionBean c where  and c.delFlag='0'";
+        //List<CompletionBean> completionlist = completionBeanDao.getAll();
+        if ("CompletionBean".equals(type)) {
+            if (questionid != null && questionid > 0) {
+                hql = "select c from CompletionBean c where c.delFlag='0' and id<>" + questionid;
+            } else {
+                hql = "select c from CompletionBean c where c.delFlag='0'";
             }
 
-            completionlist=( List<Object>)completionBeanDao.find(hql);
-        }else if("ChoiceBean".equals(type))
-        {
-            if(questionid!=null&&questionid>0)
-            {
-                hql = "select c from ChoiceBean c where  and c.delFlag='0' and id<>"+questionid ;
-            }else {
-                hql = "select c from ChoiceBean c where  c.delFlag='0'";
+            completionlist = (List<Object>) completionBeanDao.find(hql);
+        } else if ("ChoiceBean".equals(type)) {
+            if (questionid != null && questionid > 0) {
+                hql = "select c from ChoiceBean c where c.delFlag='0' and id<>" + questionid;
+            } else {
+                hql = "select c from ChoiceBean c where c.delFlag='0'";
             }
-            completionlist=( List<Object>)choiceBeanDao.find(hql);
-        }else {
-            if(questionid!=null&&questionid>0)
-            {
-                hql = "select c from VerseBean c where  and c.delFlag='0' and id<>"+questionid ;
-            }else {
+            completionlist = (List<Object>) choiceBeanDao.find(hql);
+        } else {
+            if (questionid != null && questionid > 0) {
+                hql = "select c from VerseBean c where c.delFlag='0' and id<>" + questionid;
+            } else {
                 hql = "select c from VerseBean c where c.delFlag='0'";
             }
-            completionlist=( List<Object>)verseBeanDao.find(hql);
+            completionlist = (List<Object>) verseBeanDao.find(hql);
         }
 
         referenceList.addAll(completionlist);
@@ -442,7 +436,6 @@ public class RepeatedDtoServiceImpl implements IRepeatedBeanService {
         jsonStatus.setMsg("删除成功！");
 
         return jsonStatus;
-
     }
 
 //    public static void main(String[] args) {
