b10fe205a95cce01536d80bb304335932220a5f1
houqingjie <150091225@qq.com>
2018-10-17 09:54:18 +0800

    修改角色名称

--- question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/InterviewType.java
+++ question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/InterviewType.java
@@ -12,14 +12,14 @@ public class InterviewType {
     public final static String FLIGHT_ATTENDANTS_INTERVIEW = "9";//空乘面试
     public final static String EXTEMPORE_REVIEW = "10";//即兴评述
 
-    public final static String NEWS_BROADCAST_ROLENAME = "新闻稿件播报";//新闻稿件播报
-    public final static String TOPIC_DISCUSSION_ROLENAME = "话题讨论";//话题讨论
-    public final static String PROPOSITIONAL_STORY_ROLENAME = "命题故事";//命题故事
-    public final static String PROPOSITION_TWO_PERSON_ESSAY_ROLENAME = "命题双人小品";//命题双人小品
-    public final static String ENGLISH_STUDY_ROLENAME = "英语考察";//英语考察
-    public final static String DRAMA_FILM_LITERATURE_ROLENAME = "戏剧影视文学";//戏剧影视文学
-    public final static String DRAMA_DIRECTOR_ROLENAME = "戏剧影视导演";//戏剧影视导演
-    public final static String FILM_PRODUCTION_ROLENAME = "影视摄影与制作";//影视摄影与制作
-    public final static String FLIGHT_ATTENDANTS_INTERVIEW_ROLENAME = "空乘面试";//空乘面试
-    public final static String EXTEMPORE_REVIEW_ROLENAME = "即兴评述";//即兴评述
+    public final static String NEWS_BROADCAST_ROLENAME = "新闻稿件播报审核人";//新闻稿件播报
+    public final static String TOPIC_DISCUSSION_ROLENAME = "话题讨论审核人";//话题讨论
+    public final static String PROPOSITIONAL_STORY_ROLENAME = "命题故事审核人";//命题故事
+    public final static String PROPOSITION_TWO_PERSON_ESSAY_ROLENAME = "命题双人小品审核人";//命题双人小品
+    public final static String ENGLISH_STUDY_ROLENAME = "英语考察审核人";//英语考察
+    public final static String DRAMA_FILM_LITERATURE_ROLENAME = "戏剧影视文学审核人";//戏剧影视文学
+    public final static String DRAMA_DIRECTOR_ROLENAME = "戏剧影视导演审核人";//戏剧影视导演
+    public final static String FILM_PRODUCTION_ROLENAME = "影视摄影与制作审核人";//影视摄影与制作
+    public final static String FLIGHT_ATTENDANTS_INTERVIEW_ROLENAME = "空乘面试审核人";//空乘面试
+    public final static String EXTEMPORE_REVIEW_ROLENAME = "即兴评述审核人";//即兴评述
 }
--- question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/SubjectType.java
+++ question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/SubjectType.java
@@ -11,13 +11,13 @@ public class SubjectType {
     public final static String FILM_SOUND_ANALYSIS = "8";//影片声音分析
     public final static String IMAGE_ANALYSIS = "9";//影像作品分析
 
-    public final static String SHORT_ANSWER_QUESTIONS_ROLENAME = "简答题";//简答题
-    public final static String TOPIC_DISCUSSION_ROLENAME = "论述题";//论述题
-    public final static String COMMENTARY_ROLENAME = "评述题";//评述题
-    public final static String STORY_WRITING_ROLENAME = "故事编写";//故事编写
-    public final static String STORY_RENEW_ROLENAME = "故事续写";//故事续写
-    public final static String MICRO_WRITING_ROLENAME = "微小说编写";//微小说编写
-    public final static String PHOTOGRAPHY_ANALYSIS_ROLENAME = "摄影作品分析";//摄影作品分析
-    public final static String FILM_SOUND_ANALYSIS_ROLENAME = "影片声音分析";//影片声音分析
-    public final static String IMAGE_ANALYSIS_ROLENAME = "影像作品分析";//影像作品分析
+    public final static String SHORT_ANSWER_QUESTIONS_ROLENAME = "简答题审核人";//简答题
+    public final static String TOPIC_DISCUSSION_ROLENAME = "论述题审核人";//论述题
+    public final static String COMMENTARY_ROLENAME = "评述题审核人";//评述题
+    public final static String STORY_WRITING_ROLENAME = "故事编写审核人";//故事编写
+    public final static String STORY_RENEW_ROLENAME = "故事续写审核人";//故事续写
+    public final static String MICRO_WRITING_ROLENAME = "微小说编写审核人";//微小说编写
+    public final static String PHOTOGRAPHY_ANALYSIS_ROLENAME = "摄影作品分析审核人";//摄影作品分析
+    public final static String FILM_SOUND_ANALYSIS_ROLENAME = "影片声音分析审核人";//影片声音分析
+    public final static String IMAGE_ANALYSIS_ROLENAME = "影像作品分析审核人";//影像作品分析
 }
