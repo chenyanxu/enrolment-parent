6a2e0b76ad377dae0c20b189dbb68c88aefc229b
houqingjie <150091225@qq.com>
2018-10-25 16:45:54 +0800

    新增题库功能参数设置服务

--- question-parent/enrolment-question-entities/src/main/java/com/kalix/enrolment/question/entities/QuestionSettingBean.java
+++ question-parent/enrolment-question-entities/src/main/java/com/kalix/enrolment/question/entities/QuestionSettingBean.java
@@ -1,9 +1,11 @@
 package com.kalix.enrolment.question.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kalix.framework.core.api.persistence.PersistentEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 /**
  * 题库功能参数设置实体类
@@ -12,7 +14,14 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "enrolment_question_setting")
 public class QuestionSettingBean extends PersistentEntity {
-    private Boolean available = false; // 题库功能是否开启
+    private Boolean available = false;     // 题库功能是否开启
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date questionStartTime;        // 题库功能开启时间
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date questionCloseTime;        // 题库功能关闭时间
+    private Double cilinSimilarity = 0.5;  // 题库去重词林相似度参数
+    private Double morphoSimilarity = 0.5; // 题库去重词形词序句子相似度参数
+    private Double phraseSimilarity = 0.5; // 题库去重短语相似度参数
 
     public Boolean getAvailable() {
         return available;
@@ -20,5 +29,21 @@ public class QuestionSettingBean extends PersistentEntity {
 
     public void setAvailable(Boolean available) {
         this.available = available;
+    }
+
+    public Date getQuestionStartTime() {
+        return questionStartTime;
+    }
+
+    public void setQuestionStartTime(Date questionStartTime) {
+        this.questionStartTime = questionStartTime;
+    }
+
+    public Date getQuestionCloseTime() {
+        return questionCloseTime;
+    }
+
+    public void setQuestionCloseTime(Date questionCloseTime) {
+        this.questionCloseTime = questionCloseTime;
     }
 }
