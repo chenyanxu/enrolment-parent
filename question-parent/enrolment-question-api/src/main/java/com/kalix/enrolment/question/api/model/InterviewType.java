36f6d7fb1be487f46facf14d9e2f01250e4cc51b
houqingjie <150091225@qq.com>
2018-10-25 09:01:36 +0800

    去掉面试题双人小品试题类型

--- question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/InterviewType.java
+++ question-parent/enrolment-question-api/src/main/java/com/kalix/enrolment/question/api/model/InterviewType.java
@@ -4,7 +4,7 @@ public class InterviewType {
     public final static String NEWS_BROADCAST = "1";//新闻稿件播报
     public final static String TOPIC_DISCUSSION = "2";//话题讨论
     public final static String PROPOSITIONAL_STORY = "3";//命题故事
-    public final static String PROPOSITION_TWO_PERSON_ESSAY = "4";//命题双人小品
+//    public final static String PROPOSITION_TWO_PERSON_ESSAY = "4";//命题双人小品
     public final static String ENGLISH_STUDY = "5";//英语考察
     public final static String DRAMA_FILM_LITERATURE = "6";//戏剧影视文学
     public final static String DRAMA_DIRECTOR = "7";//戏剧影视导演
@@ -15,7 +15,7 @@ public class InterviewType {
     public final static String NEWS_BROADCAST_ROLENAME = "新闻稿件播报审核人";//新闻稿件播报
     public final static String TOPIC_DISCUSSION_ROLENAME = "话题讨论审核人";//话题讨论
     public final static String PROPOSITIONAL_STORY_ROLENAME = "命题故事审核人";//命题故事
-    public final static String PROPOSITION_TWO_PERSON_ESSAY_ROLENAME = "命题双人小品审核人";//命题双人小品
+//    public final static String PROPOSITION_TWO_PERSON_ESSAY_ROLENAME = "命题双人小品审核人";//命题双人小品
     public final static String ENGLISH_STUDY_ROLENAME = "英语考察审核人";//英语考察
     public final static String DRAMA_FILM_LITERATURE_ROLENAME = "戏剧影视文学审核人";//戏剧影视文学
     public final static String DRAMA_DIRECTOR_ROLENAME = "戏剧影视导演审核人";//戏剧影视导演
