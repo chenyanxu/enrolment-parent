d32c9a19d8362607574e103754b0abcca64c0263
houqingjie <150091225@qq.com>
2019-02-25 09:47:55 +0800

    新增考生信息和考试成绩服务发布

--- examinee-parent/enrolment-examinee-biz/src/main/java/com/kalix/enrolment/examinee/biz/ExamScoreBeanServiceImpl.java
+++ examinee-parent/enrolment-examinee-biz/src/main/java/com/kalix/enrolment/examinee/biz/ExamScoreBeanServiceImpl.java
@@ -3,6 +3,8 @@ package com.kalix.enrolment.examinee.biz;
 import com.kalix.enrolment.examinee.api.biz.IExamScoreBeanService;
 import com.kalix.enrolment.examinee.api.dao.IExamScoreBeanDao;
 import com.kalix.enrolment.examinee.entities.ExamScoreBean;
+import com.kalix.framework.core.api.persistence.JsonStatus;
+import com.kalix.framework.core.util.Assert;
 import com.kalix.framework.extend.impl.biz.LogicDeleteGenericBizServiceImpl;
 
 import java.util.List;
@@ -11,6 +13,31 @@ import java.util.List;
  * Created by zangyanming at 2018-09-13
  */
 public class ExamScoreBeanServiceImpl extends LogicDeleteGenericBizServiceImpl<IExamScoreBeanDao, ExamScoreBean> implements IExamScoreBeanService {
+    private static final String FUNCTION_NAME = "考试成绩";
 
+    @Override
+    public boolean isSave(ExamScoreBean entity, JsonStatus status) {
+        ExamScoreBean bean = (ExamScoreBean) entity;
+        List<ExamScoreBean> examScoreBeens = dao.find("select ob from ExamScoreBean ob where ob.barCode = ?1", bean.getBarCode());
+        if (examScoreBeens != null && examScoreBeens.size() > 0) {
+            status.setFailure(true);
+            status.setMsg(FUNCTION_NAME + "已经存在！");
+            return false;
+        }
+        return true;
+    }
 
+    @Override
+    public boolean isUpdate(ExamScoreBean entity, JsonStatus status) {
+        Assert.notNull(entity, "实体不能为空.");
+        ExamScoreBean bean = (ExamScoreBean) entity;
+        List<ExamScoreBean> examScoreBeens = dao.find("select ob from ExamScoreBean ob where ob.id <> ?1 and ob.barCode = ?2",
+                bean.getId(), bean.getBarCode());
+        if (examScoreBeens != null && examScoreBeens.size() > 0) {
+            status.setFailure(true);
+            status.setMsg(FUNCTION_NAME + "已经存在！");
+            return false;
+        }
+        return true;
+    }
 }
